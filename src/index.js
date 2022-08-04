5dac58fb478c0af2feffe5bd75f11c3d5d833f36
Mauro Alderete <alderete.mauro@gmail.com>
2022-08-04 18:59:45 -0300

    fix: input name and add config element

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const IntegerActionInput = require('./actionInputs/integerActionInput');
 const StringActionInput = require('./actionInputs/stringActionInput');
 const YAMLActionInput = require('./actionInputs/YAMLActionInput');
 
+const inputPullRequestNumber = 'pull-request-number';
+const inputGithubToken = 'github-token';
+const inputCleanLabels = 'clear-all-labels-to-start';
+const inputRemoveLabelsNotFound = 'maintain-labels-not-matched';
+const inputApplyChanges = 'apply-changes';
+const inputConventionalCommits = 'conventional-commits';
+
 try {
   core.setOutput('labels-before-update', []);
   core.setOutput('labels-added', []);
@@ -16,12 +23,30 @@ try {
   core.setOutput('action-status', 'STARTED');
   core.setOutput('action-message', '');
 
-  const pullRequestNumber = new IntegerActionInput(core.getInput('pull-request-number'));
-  const githubToken = new StringActionInput(core.getInput('github-token'));
-  const cleanLabels = new BoolActionInput(core.getInput('clear-all-labels-to-start'));
-  const removeLabelsNotFound = new BoolActionInput(core.getInput('remove-labels-not-found'));
-  const notApplyChanges = new BoolActionInput(core.getInput('not-apply-changes'));
-  const conventionalCommits = new YAMLActionInput(core.getInput('conventional-commits'));
+  const pullRequestNumber = new IntegerActionInput(
+    core.getInput(inputPullRequestNumber),
+    { id: inputPullRequestNumber },
+  );
+  const githubToken = new StringActionInput(
+    core.getInput(inputGithubToken),
+    { id: inputGithubToken },
+  );
+  const cleanLabels = new BoolActionInput(
+    core.getInput(inputCleanLabels),
+    { id: inputCleanLabels },
+  );
+  const removeLabelsNotFound = new BoolActionInput(
+    core.getInput(inputRemoveLabelsNotFound),
+    { id: inputRemoveLabelsNotFound },
+  );
+  const ApplyChanges = new BoolActionInput(
+    core.getInput(inputApplyChanges),
+    { id: inputApplyChanges },
+  );
+  const conventionalCommits = new YAMLActionInput(
+    core.getInput(inputConventionalCommits),
+    { id: inputConventionalCommits },
+  );
 
   core.setOutput('action-status', 'VALIDATED');
 
