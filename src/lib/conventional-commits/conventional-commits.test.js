2e610c4df3525c5d1b0439511653063934b8c8b2
Mauro Alderete <alderete.mauro@gmail.com>
2022-08-13 20:49:44 -0300

    refact: simplify test conventional-commits removing dependency

--- src/lib/conventional-commits/conventional-commits.test.js
+++ src/lib/conventional-commits/conventional-commits.test.js
@@ -1,25 +1,5 @@
 /* eslint-disable no-unused-vars */
 const { getTypesInCommits } = require('./conventional-commits');
-const { makeYAMLLoader } = require('../yaml-loader/yaml-loader');
-
-const readerMock = (path, options) => {
-  if (path !== 'conventional-commits.yml') {
-    throw new Error('file not found');
-  }
-
-  return `conventional-commits:
-  - type: 'fix'
-    nouns: ['fix', 'fixed']
-    labels: ['bug']
-  - type: 'feature'
-    nouns: ['feat', 'feature']
-    labels: ['enhancement']
-  - type: 'breaking_change'
-    nouns: ['BREAKING CHANGE']
-    labels: ['BREAKING CHANGE']`;
-};
-
-const yamlLoader = makeYAMLLoader(readerMock);
 
 describe('conventional-commits', () => {
   const commits = [
@@ -33,7 +13,39 @@ describe('conventional-commits', () => {
     'feat: get labels before update',
   ];
 
-  const ccScheme = yamlLoader('conventional-commits.yml');
+  const conventionalCommits = {
+    'conventional-commits': [
+      {
+        type: 'fix',
+        nouns: [
+          'fix',
+          'fixed',
+        ],
+        labels: [
+          'bug',
+        ],
+      },
+      {
+        type: 'feature',
+        nouns: [
+          'feat',
+          'feature',
+        ],
+        labels: [
+          'enhancement',
+        ],
+      },
+      {
+        type: 'breaking_change',
+        nouns: [
+          'BREAKING CHANGE',
+        ],
+        labels: [
+          'BREAKING CHANGE',
+        ],
+      },
+    ],
+  };
 
   it('get conventional-commits types from commits using conventional-commits scheme', () => {
     const expected = [
@@ -41,6 +53,6 @@ describe('conventional-commits', () => {
       { type: 'feature', nouns: ['feat', 'feature'], labels: ['enhancement'] },
     ];
 
-    expect(getTypesInCommits(commits, ccScheme)).toEqual(expected);
+    expect(getTypesInCommits(commits, conventionalCommits)).toEqual(expected);
   });
 });
