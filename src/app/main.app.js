2e25fc0dd30f92f7a3a4246b7f85f46ff5b224bc
Mauro Alderete <alderete.mauro@gmail.com>
2022-08-12 23:40:27 -0300

    feat: add first approach to main app with unit test

--- src/app/main.app.js
+++ src/app/main.app.js
@@ -2,16 +2,12 @@ const { makeInputConfigurator } = require('../actionInputs/inputConfigurator');
 const { makeInputGetter } = require('../actionInputs/inputGetter');
 const { makeInputsLoader } = require('./inputsLoader.app');
 
-const mainApp = (githubCore) => () => {
+const mainApp = (githubCore, yamlLoaderClient) => () => {
   const inputConfigurator = makeInputConfigurator();
-  console.log('init configurator', inputConfigurator);
-  const getInput = makeInputGetter(githubCore.getInput, inputConfigurator);
-  console.log('init getInput', getInput);
-  const loadInputs = makeInputsLoader(getInput);
-  console.log('init loadInputs', loadInputs);
 
-  const inputs = loadInputs();
-  console.log('inputs loaded: ', inputs);
+  const getInput = makeInputGetter(githubCore.getInput, inputConfigurator);
+  const loadInputs = makeInputsLoader(getInput, yamlLoaderClient);
+  loadInputs();
 };
 
 module.exports = { mainApp };
--- src/app/main.app.test.js
+++ src/app/main.app.test.js
@@ -1,17 +1,28 @@
 const { mainApp } = require('./main.app');
+const { makeYAMLLoader } = require('./YAMLLoader.app');
 
 const coreMock = {
   getInput: (name) => {
-    if (name === 'pull-request-number') {
-      return '0';
-    }
-    return undefined;
+    const inputs = {
+      'pull-request-number': '100',
+      'github-token': 'asd',
+      'maintain-labels-not-matched': false,
+      'apply-changes': true,
+      'conventional-commits': `%YAML 1.2
+      conventional-commits:
+      - type: 'fix'
+        nouns: ['fix', 'fixed']
+        labels: ['bug']
+      `,
+    };
+    return inputs[name];
   },
 };
 
 describe('Main App', () => {
   it('run', () => {
-    const app = mainApp(coreMock);
+    const yamlLoaderClient = makeYAMLLoader();
+    const app = mainApp(coreMock, yamlLoaderClient);
     console.log('run app');
     app();
     expect(1).toBe(1);
