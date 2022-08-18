bfefd5452c6ab6b0d52800e673d30c466f9462ee
Mauro Alderete <alderete.mauro@gmail.com>
2022-08-18 12:05:39 -0300

    doc: document index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+/**
+ * The index is the action's entry point.
+ * Only contains a single call to the [app module]{@link module:src/app/app} to start the action.
+ * <p>This extra layer before running the action's core allows
+ * separate the entry point to the production of the rest of all modules
+ * when are used to run tests.
+ * In this way doesn't need to make any specific environment or
+ * environment variables or another approach
+ * that will affect the user requirements and experiences.</p>
+ * @module src/index
+ */
+
 /* istanbul ignore next */
 const app = require('./app/app');
 
